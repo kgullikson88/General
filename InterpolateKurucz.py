63e1b5c5887f0747c6793025367e95778939bb52
kgullikson <kgullikson@d80f6419-c1d6-4522-9ad1-6f7b80fa307c>
2012-11-05 17:25:27 +0000

    InterpolateKurucz now does interpolation over metallicity as well

--- InterpolateKurucz.py
+++ InterpolateKurucz.py
@@ -12,43 +12,49 @@ model_directory = homedir + "Dropbox/School/Research/AstarStuff/TargetLists/AgeD
 
 class Models:
   def __init__(self, modeldir=model_directory, debug=False):
-    self.model_dict = defaultdict(str)   #Dictionary of all models in modeldir, with temperature as key
-    self.read_dict = defaultdict(list)   #Dictionary of all models already read in.
-                                         #The list is the wavelength, followed by a dictionary
-                                         #of flux values for each value of log(g)
+    self.model_dict = defaultdict(lambda : defaultdict(str))   #Dictionary of all models in modeldir,
+                                                               #with temperature and metallicity as key
+    self.read_dict = defaultdict(lambda : defaultdict(str))    #Dictionary of all models already read in.
+                                                               #The list is the wavelength, followed by
+                                                               #a dictionary of flux values for each
+                                                               #value of log(g)
     self.logg_grid = numpy.arange(0.0, 5.5, 0.5)  #grid of log(g) values for the default Kurucz grid.
                                                   #Warning! May not be true if a different grid is used!
     self.modeldir = modeldir
     self.debug = debug
     allfiles = os.listdir(modeldir)
     for fname in allfiles:
-      if fname.startswith("ckp00") and fname.endswith(".fits"):
+      if fname.startswith("ck") and fname.endswith(".fits"):
         temperature = float(fname.split("_")[-1].split(".fits")[0])
-        self.model_dict[temperature] = fname
+        metallicity_string = fname[2:5]
+        metallicity = float(metallicity_string[1:])/10.0
+        if "m" in metallicity_string:
+          metallicity *= -1
+        self.model_dict[temperature][metallicity] = fname
 
   #Function to read in a model with temperature T
-  def ReadFile(self, T, logg):
+  def ReadFile(self, T, logg, metal):
     #Make sure it is not already read in
-    if T in self.read_dict.keys():
-      if logg in self.read_dict[T][1].keys():
+    if T in self.read_dict.keys() and metal in self.read_dict[T].keys():
+      if logg in self.read_dict[T][metal][1].keys():
         if self.debug:
           print "Model with T = %g and log(g) = %g already read. Skipping..." %(T, logg)
         return 0
       else:
-        hdulist = pyfits.open(self.modeldir + "/" + self.model_dict[T])
+        hdulist = pyfits.open(self.modeldir + "/" + self.model_dict[T][metal])
         data = hdulist[1].data
         hdulist.close()
         logg_str = "g%.2i" %(logg*10.)
         try:
           flux = data[logg_str]
-          self.read_dict[T][1][logg] = flux
+          self.read_dict[T][metal][1][logg] = flux
         except KeyError:
-          print "Error! log(g) = %g does not exist in file %s!" %(logg, self.modeldir + "/" + self.model_dict[T])
+          print "Error! log(g) = %g does not exist in file %s!" %(logg, self.modeldir + "/" + self.model_dict[T][metal])
           return -1
     else:
-      #This means this temperature has not yet been read in
-      fname = self.modeldir + "/" + self.model_dict[T]
-      if self.model_dict[T] == "":
+      #This means this temperature and metallicity has not yet been read in
+      fname = self.modeldir + "/" + self.model_dict[T][metal]
+      if self.model_dict[T][metal] == "":
         #This temperature does not exist in the model
         print "Error! No model found with T = %g" %T
         return -1
@@ -62,7 +68,7 @@ class Models:
         try:
           flux = data[logg_str]
           d[logg] = flux
-          self.read_dict[T] = [wave, d]
+          self.read_dict[T][metal] = [wave, d]
         except KeyError:
           print "Error! log(g) = %g does not exist in file %s!" %(logg, fname)
           return -1
@@ -72,18 +78,18 @@ class Models:
 
 
   #Function to retrieve data, returning as an xypoint
-  def GetSpectrum(self, T, logg):
-    retval = self.ReadFile(T, logg)
+  def GetSpectrum(self, T, logg, metal):
+    retval = self.ReadFile(T, logg, metal)
     if retval == 0:
-      wave = self.read_dict[T][0]
-      flux = self.read_dict[T][1][logg]
+      wave = self.read_dict[T][metal][0]
+      flux = self.read_dict[T][metal][1][logg]
       return DataStructures.xypoint(x=wave, y=flux)
     else:
-      return -1
+      return retval
   
 
   #Function to linearly interpolate to a given Temperature and log(g)
-  def GetInterpolatedSpectrum(self, T, logg):
+  def GetInterpolatedSpectrum(self, T, logg, metal=0.0):
     #First, find the closest two temperatures in self.model_dict
     Tclosest = 9e9
     Tsecond = 0
@@ -103,39 +109,100 @@ class Models:
 
     #Do the same thing for log(g)
     gclosest = 9e9
-    gsecond = 9e10
+    gsecond =0
     for g in self.logg_grid:
       if numpy.abs(logg-g) < numpy.abs(gclosest-g):
         gclosest=g
-    for g in self.logg_grid:
-      if numpy.abs(logg-g) < numpy.abs(gsecond-g) and g != gclosest:
-        gsecond=g
-        
+    i = 0
+    while gsecond < gclosest:
+      gsecond = self.logg_grid[i]
+      i += 1
+    if gclosest > logg and i > 1:
+      gsecond = self.logg_grid[i-2]
 
-    #For each temperature, we will interpolate to the requested log(g) first
-    #Do the closest temperature first
-    spec1 = self.GetSpectrum(Tclosest, gclosest)
-    spec2 = self.GetSpectrum(Tclosest, gsecond)
+    #And again for metallicity
+    metalclosest = 9e9
+    metalsecond = -9e9
+    metals = sorted(self.model_dict[Tclosest].keys())
+    if self.debug:
+      print "Metals list: "
+      print metals
+    for z in metals:
+      if numpy.abs(metal - z) < numpy.abs(metalclosest - z):
+        metalclosest = z
+    i = 0
+    while metalsecond < metalclosest:
+      metalsecond = metals[i]
+      i += 1
+    if metalclosest > metal and i > 1:
+      metalsecond = metals[i-2]
+
+    if self.debug:
+      print "T = %g\tlog(g) = %g\t[Z/H] = %g" %(T, logg, metal)
+      print "[%g, %g]\t[%g, %g]\t[%g, %g]" %(Tclosest, Tsecond, gclosest, gsecond, metalclosest, metalsecond)
+
+    #For each temperature and metallicity, we will interpolate to the requested log(g) first
+    #Do the closest temperature and metallicity first
+    spec1 = self.GetSpectrum(Tclosest, gclosest, metalclosest)
+    spec2 = self.GetSpectrum(Tclosest, gsecond, metalclosest)
     if logg == gclosest:
-      spectrum_T1 = spec1.copy()
+      spectrum_T1_Z1 = spec1.copy()
     elif type(spec1) != int and type(spec2) != int:
       #This means everything went fine with GetSpectrum
-      spectrum_T1 = spec1.copy()
-      spectrum_T1.y = (spec2.y - spec1.y)/(gsecond - gclosest)*(logg - gclosest) + spec1.y
+      spectrum_T1_Z1 = spec1.copy()
+      spectrum_T1_Z1.y = (spec2.y - spec1.y)/(gsecond - gclosest)*(logg - gclosest) + spec1.y
+    else:
+      return -1
+
+    #Closest Temperature, second closest metallicity
+    spec1 = self.GetSpectrum(Tclosest, gclosest, metalsecond)
+    spec2 = self.GetSpectrum(Tclosest, gsecond, metalsecond)
+    if logg == gclosest:
+      spectrum_T1_Z2 = spec1.copy()
+    elif type(spec1) != int and type(spec2) != int:
+      #This means everything went fine with GetSpectrum
+      spectrum_T1_Z2 = spec1.copy()
+      spectrum_T1_Z2.y = (spec2.y - spec1.y)/(gsecond - gclosest)*(logg - gclosest) + spec1.y
     else:
       return -1
       
-    #And the second closest temperature
-    spec1 = self.GetSpectrum(Tsecond, gclosest)
-    spec2 = self.GetSpectrum(Tsecond, gsecond)
+    #And the second closest temperature with closest metallicity
+    spec1 = self.GetSpectrum(Tsecond, gclosest, metalclosest)
+    spec2 = self.GetSpectrum(Tsecond, gsecond, metalclosest)
     if logg == gclosest:
-      spectrum_T2 = spec1.copy()
+      spectrum_T2_Z1 = spec1.copy()
     if type(spec1) != int and type(spec2) != int:
       #This means everything went fine with GetSpectrum
-      spectrum_T2 = spec1.copy()
-      spectrum_T2.y = (spec2.y - spec1.y)/(gsecond - gclosest)*(logg - gclosest) + spec1.y
+      spectrum_T2_Z1 = spec1.copy()
+      spectrum_T2_Z1.y = (spec2.y - spec1.y)/(gsecond - gclosest)*(logg - gclosest) + spec1.y
     else:
       return -1
+
+    #Finally, the second closest temperature with the second closest metallicity
+    spec1 = self.GetSpectrum(Tsecond, gclosest, metalsecond)
+    spec2 = self.GetSpectrum(Tsecond, gsecond, metalsecond)
+    if logg == gclosest:
+      spectrum_T2_Z2 = spec1.copy()
+    if type(spec1) != int and type(spec2) != int:
+      #This means everything went fine with GetSpectrum
+      spectrum_T2_Z2 = spec1.copy()
+      spectrum_T2_Z2.y = (spec2.y - spec1.y)/(gsecond - gclosest)*(logg - gclosest) + spec1.y
+    else:
+      return -1
+
+    #Now, interpolate to the requested metallicity
+    spectrum_T1 = spectrum_T1_Z1.copy()
+    if metalclosest != metal and numpy.all(spectrum_T1_Z1.x == spectrum_T1_Z2.x):
+      spectrum_T1.y = (spectrum_T1_Z1.y - spectrum_T1_Z2.y)/(metalclosest - metalsecond) * (metal - metalclosest) + spectrum_T1_Z1.y
+    else:
+      return -1
+
+    spectrum_T2 = spectrum_T2_Z1.copy()
+    if metalclosest != metal and numpy.all(spectrum_T2_Z1.x == spectrum_T2_Z2.x):
+      spectrum_T2.y = (spectrum_T2_Z1.y - spectrum_T2_Z2.y)/(metalclosest - metalsecond) * (metal - metalclosest) + spectrum_T2_Z1.y
+    else:
+      return -1
+      
 
     spectrum = spectrum_T1.copy()
     if T == Tclosest:
