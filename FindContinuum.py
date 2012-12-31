08557fbf2b5d506aa2f499a9e1bf5fbd59ee9a9f
kgullikson <kgullikson@d80f6419-c1d6-4522-9ad1-6f7b80fa307c>
2012-12-31 03:32:29 +0000

    Edited FindContinuum.py. Minor changes I think

--- FindContinuum.py
+++ FindContinuum.py
@@ -1,7 +1,11 @@
 import numpy
+from scipy.interpolate import InterpolatedUnivariateSpline as spline
 
-
-
+"""
+  This function fits the continuum spectrum by iteratively removing
+points over one standard deviation below the mean, which are assumed
+to be absorption lines.
+"""
 def Continuum(x, y, fitorder=3):
   done = False
   x2 = numpy.copy(x)
@@ -18,3 +22,33 @@ def Continuum(x, y, fitorder=3):
       x2 = numpy.delete(x2, badpoints)
       y2 = numpy.delete(y2, badpoints)
   return fit(x - x2.mean())
+
+
+"""
+  This function does a boxcar smoothing instead of fitting to a polynomial as above.
+"""
+def IterativeBoxcarSmooth(x, y, bcwidth=50, numstd=2.0):
+  boxcar = numpy.ones(bcwidth)/float(bcwidth)
+  done = False
+  sizes = []    #To check if it gets stuck
+  x2 = numpy.array(x)
+  y2 = numpy.array(y)
+  print "Number of standard deviations = %.2g" %numstd
+  while not done:
+    after = y2[-boxcar.size/2+1:]
+    before = y2[:boxcar.size/2]
+    extended = numpy.append(numpy.append(before, y2), after)
+    smoothed = numpy.convolve(extended, boxcar, mode='valid')
+    residuals = y2/smoothed - 1.0
+    std = numpy.std(residuals)
+    if std < 1e-8:
+      std = 1e-8
+    badindices = numpy.where(numpy.logical_and(residuals < -numstd*std, residuals > numstd*std))[0]
+    if badindices.size < 5 or (len(sizes) > 10 and numpy.all(size == badindices.size for size in sizes[-10:])):
+      done = True
+      break
+    sizes.append(badindices.size)
+    goodindices = numpy.where(numpy.logical_and(residuals >= -numstd*std, residuals <= numstd*std))[0]
+    fcn = spline(x2[goodindices], y2[goodindices], k=1)
+    y2 = fcn(order2.x)
+  return smoothed
