1ffebe324148b3ff7d2c4748318871e631eb09a1
kgullikson88 <kevin.gullikson@gmail.com>
2014-09-11 14:11:50 -0500

    Pre main sequence tracks are just not good enough, because the temperature is systematically over-estimated, and the radius is systematically under-estimated. Therefore, I have gone back to just using main sequence relationships.

--- Sensitivity.py
+++ Sensitivity.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import sys
 import FittingUtilities
@@ -6,7 +5,6 @@ from re import search
 
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline as interp
-
 from astropy.io import fits
 from astropy.io import ascii
 from astropy import units, constants
@@ -23,13 +21,9 @@ import Correlate
 
 
 MS = SpectralTypeRelations.MainSequence()
-PMS = SpectralTypeRelations.PreMainSequence(
-    pms_tracks_file="%s/Dropbox/School/Research/Stellar_Evolution/Baraffe_Tracks.dat" % (os.environ["HOME"]),
-    track_source="Baraffe")
-PMS2 = SpectralTypeRelations.PreMainSequence()
 
 
-def GetFluxRatio(sptlist, Tsec, xgrid, age=None):
+def GetFluxRatio(sptlist, Tsec, xgrid):
     """
       Returns the flux ratio between the secondary star of temperature Tsec
       and the (possibly multiple) primary star(s) given in the
@@ -37,71 +31,35 @@ def GetFluxRatio(sptlist, Tsec, xgrid, age=None):
       xgrid is a np.ndarray containing the x-coordinates to find the
         flux ratio at (in nm)
 
-      The age of the system is found from the main-sequence age of the
-        earliest spectral type in sptlist, if it is not given
     """
     prim_flux = np.zeros(xgrid.size)
-    sec_flux = np.zeros(xgrid.size)
 
-    # First, get the age of the system
-    if age is None:
-        age = GetAge(sptlist)
-
-    # Now, determine the flux from the primary star(s)
+    # Determine the flux from the primary star(s)
     for spt in sptlist:
         end = search("[0-9]", spt).end()
         T = MS.Interpolate(MS.Temperature, spt[:end])
-        R = PMS2.GetFromTemperature(age, T, key="Radius")
+        R = MS.Interpolate(MS.Radius, spt[:end])
         prim_flux += Planck(xgrid * units.nm.to(units.cm), T) * R ** 2
 
     # Determine the secondary star flux
-    R = PMS.GetFromTemperature(age, Tsec, key="Radius")
+    s_spt = MS.GetSpectralType(MS.Temperature, Tsec)
+    R = MS.Interpolate(MS.Radius, s_spt)
     sec_flux = Planck(xgrid * units.nm.to(units.cm), Tsec) * R ** 2
 
     return sec_flux / prim_flux
 
 
-def GetMass(spt, age):
+def GetMass(spt):
     """
     Returns the mass of the system in solar masses
     spt: Spectral type of the star
-    age: age, in years, of the system
     """
 
     # Get temperature
     end = search("[0-9]", spt).end()
-    T = MS.Interpolate(MS.Temperature, spt[:end])
-
-    # Determine which tracks to use
-    if spt[0] == "O" or spt[0] == "B" or spt[0] == "A" or spt[0] == "F":
-        return PMS2.GetFromTemperature(age, T, key="Mass")
-    else:
-        return PMS.GetFromTemperature(age, T, key="Mass")
+    return MS.Interpolate(MS.Mass, spt[:end])
 
 
-def GetAge(sptlist):
-    """
-    Returns the age of the system, in years, given a list
-    of spectral types. It determines the age as the
-    main sequence lifetime of the earliest-type star
-    """
-
-    lowidx = 999
-    for spt in sptlist:
-        if "I" in spt:
-            # Pre-main sequence. Just use the age of an early O star,
-            # which is ~ 1Myr
-            lowidx = 1
-            break
-        end = search("[0-9]", spt).end()
-        idx = MS.SpT_To_Number(spt[:end])
-        if idx < lowidx:
-            lowidx = idx
-
-    spt = MS.Number_To_SpT(lowidx)
-    Tprim = MS.Interpolate(MS.Temperature, spt)
-    age = PMS2.GetMainSequenceAge(Tprim, key="Temperature")
-    return age
 
 
 def Analyze(fileList,
@@ -177,6 +135,7 @@ def Analyze(fileList,
             outfile = open(logfilenames[fname], "a")
 
             # Read in and process the data like I am about to look for a companion
+            # TODO: We should be removing the bad regions AFTER adding the model!
             orders_original = GenericSearch.Process_Data(fname, badregions=badregions, trimsize=trimsize)
 
             #Find the vsini of the primary star with my spreadsheet
@@ -208,13 +167,12 @@ def Analyze(fileList,
                 sys.exit("Star ({:s}) not found in multiplicity library ({:s}!".format(starname, companion_file))
 
             # Determine the age of the system and properties of the primary and secondary star
-            age = GetAge(known_stars)
             primary_spt = known_stars[0]
             end = search("[0-9]", primary_spt).end()
             primary_temp = MS.Interpolate(MS.Temperature, primary_spt[:end])
-            primary_mass = GetMass(primary_spt, age)
+            primary_mass = GetMass(primary_spt)
             secondary_spt = MS.GetSpectralType(MS.Temperature, temp)
-            secondary_mass = GetMass(secondary_spt, age)
+            secondary_mass = GetMass(secondary_spt)
             massratio = secondary_mass / primary_mass
 
             for rv in vel_list:
@@ -223,7 +181,7 @@ def Analyze(fileList,
                 model_orders = []
                 for ordernum, order in enumerate(orders):
                     # Get the flux ratio
-                    scale = GetFluxRatio(known_stars, temp, order.x, age=age)
+                    scale = GetFluxRatio(known_stars, temp, order.x)
                     if debug:
                         print "Scale factor for order %i is %.3g" % (ordernum, scale.mean())
 
