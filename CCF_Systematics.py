d2b516a4ad8dff8fd9652a23708468176666e8d0
kgullikson88 <kevin.gullikson@gmail.com>
2015-02-16 14:00:06 -0600

    Do not return twice the measured error

--- CCF_Systematics.py
+++ CCF_Systematics.py
@@ -252,17 +252,7 @@ def make_gaussian_process_samples(df):
                  and 'Tactual' (for the actual temperature)
     :return: emcee sampler instance
     """
-    # First, find the uncertainties at each actual temperature
-    # Tactual = df['Tactual'].values
-    #Tmeasured = df['Temperature'].values
-    #error = df['Tact_err'].values
-    #temp = df.groupby('Temperature').mean()['Tactual']
-    #Tmeasured = temp.keys().values
-    #Tactual = temp.values
-    #error = np.nan_to_num(df.groupby('Temperature').std(ddof=1)['Tactual'].values)
     Tmeasured, Tactual, error, lit_err = get_values(df)
-    #default = max(150.0, np.median(error[error > 1]))
-    #error = np.maximum(error, np.ones(error.size) * default)
     for i, e in enumerate(error):
         if e < 1:
             e = fit_sigma(df, i)
@@ -460,9 +450,9 @@ def fit_sigma(df, i):
     values, _= np.histogram(good.Tactual.values, bins=bins)
     
     mean = np.mean(good.Tactual.values)
-    std = np.std(good.Tactual.values)
+    std = np.std(good.Tactual.values, ddof=1)
     if std > 0:
-        return mean, std
+        return std
     
     sigma_test = np.arange(500, 10, -10) #Just test a bunch of values
     idx = np.searchsorted(bins, mean)
@@ -474,7 +464,7 @@ def fit_sigma(df, i):
     probs = [get_probability(x1, x2, x3, x4, N, mean, s) for s in sigma_test]
     for s, p in zip(sigma_test, probs):
         if p > 0.5:
-            return s * 2
+            return s
     
     raise ValueError('No probability > 0!')
 
