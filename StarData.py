3bbd19783a4c386b3cc6501035464087b771eee9
kgullikson88 <kevin.gullikson@gmail.com>
2015-02-19 19:33:52 -0600

    Moved get_primary_vsini to StarData

--- StarData.py
+++ StarData.py
@@ -1,6 +1,9 @@
 import sys
 
 from astroquery.simbad import Simbad
+import os
+import pandas as pd
+from astropy.io import fits
 
 Simbad.add_votable_fields('sp', 'flux(V)', 'plx')
 
@@ -30,6 +33,29 @@ def GetData(starname):
     data_cache[starname] = data
     return data
 
+
+def get_vsini(file_list):
+    homedir = os.environ['HOME']
+    vsini = pd.read_csv("{}/School/Research/Useful_Datafiles/Vsini.csv".format(homedir), sep='|', skiprows=8)[1:]
+    vsini_dict = {}
+    prim_vsini = []
+    for fname in file_list:
+        root = fname.split('/')[-1][:9]
+        if root in vsini_dict:
+            prim_vsini.append(vsini_dict[root])
+        else:
+            header = fits.getheader(fname)
+            star = header['OBJECT']
+            try:
+                v = vsini.loc[vsini.Identifier.str.strip() == star]['vsini(km/s)'].values[0]
+                prim_vsini.append(float(v) * 0.8)
+                vsini_dict[root] = float(v) * 0.8
+            except IndexError:
+                logging.warn('No vsini found for star {}! No primary star removal will be attempted!'.format(star))
+                prim_vsini.append(None)
+    for fname, vsini in zip(file_list, prim_vsini):
+        print fname, vsini
+    return prim_vsini
 
 if __name__ == "__main__":
     for starname in sys.argv[1:]:
