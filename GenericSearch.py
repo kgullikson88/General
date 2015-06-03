Fixed off-by-one error

GenericSearch.py
@@ -134,7 +134,7 @@ def Process_Data(fname, badregions=[], interp_regions=[], extensions=True,
             order = FittingUtilities.RebinData(order, xgrid)
 
             dx = order.x[1] - order.x[0]
-            smooth_factor = 0.8
+            smooth_factor = 0.5
             """ old method
             theta = GenericSmooth.roundodd(vsini / 3e5 * order.x.mean() / dx * smooth_factor)
             theta = max(theta, 21)
@@ -249,8 +249,7 @@ def process_model(model, data, vsini_model=None, resolution=None, vsini_primary=
 
     # Divide by the same smoothing kernel as we used for the data
     if vsini_primary is not None:
-
-        smooth_factor = 0.8
+        smooth_factor = 0.5
         """ old method
         d_logx = np.log(xgrid[1] / xgrid[0])
         theta = GenericSmooth.roundodd(vsini_primary / 3e5 * smooth_factor / d_logx)

HelperFunctions.py
@@ -5,17 +5,17 @@
 import os
 import csv
 from collections import defaultdict
+
 from scipy.optimize import bisect
 from scipy.stats import scoreatpercentile
 from scipy.signal import kaiserord, firwin, lfilter
 from scipy.interpolate import InterpolatedUnivariateSpline as spline, UnivariateSpline
-
 from astropy.io import fits as pyfits
 import numpy as np
 from astropy import units, constants
 from astropy.time import Time
-import DataStructures
 
+import DataStructures
 import pySIMBAD as sim
 import SpectralTypeRelations
 import readmultispec as multispec
@@ -568,6 +568,7 @@ def HighPassFilter(data, vel, width=5, linearize=False):
     # The phase delay of the filtered signal.
     delay = 0.5 * (N - 1) / sample_rate
     delay_idx = np.searchsorted(data.x, data.x[0] + delay) - 1
+    delay_idx = np.searchsorted(data.x, data.x[0] + delay)
     smoothed_y = smoothed_y[data.size() + delay_idx:-data.size() + delay_idx]
     if linearize:
         return linear.x, smoothed_y