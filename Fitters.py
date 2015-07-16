8a28227be258331d742554111afcaeb1e9dd105a
kgullikson88 <kevin.gullikson@gmail.com>
2015-07-16 09:51:44 -0500

    Refactored Bayesian_LS class to allow for either emcee or pymultinest backends. Should be able to get rid of the MultinestFitter stuff now!

--- Fitters.py
+++ Fitters.py
@@ -5,9 +5,12 @@ A set of functions for various types of fitting.
 import logging
 import FittingUtilities
 from george import kernels
+import os
+import glob
+import json
 
 from scipy.optimize import fmin, brute
-from scipy.interpolate import interp1d, InterpolatedUnivariateSpline as spline
+from scipy.interpolate import InterpolatedUnivariateSpline as spline
 from scipy.stats import norm
 import numpy as np
 from lmfit import Model, Parameters
@@ -16,18 +19,15 @@ import matplotlib.pyplot as plt
 import george
 import statsmodels.api as sm
 from statsmodels.robust.norms import TukeyBiweight
-import os
-import glob
-import json
 import pandas as pd
 import triangle
-
-import DataStructures
-from HelperFunctions import IsListlike, ExtrapolatingUnivariateSpline, ensure_dir, fwhm
-from FittingUtilities import bound
 from astropy import units as u, constants
 from astropy.modeling import fitting
 from astropy.modeling.polynomial import Chebyshev2D
+
+import DataStructures
+from HelperFunctions import IsListlike, ExtrapolatingUnivariateSpline, ensure_dir, fwhm
+
 #from astropy.analytic_functions import blackbody_lambda as blackbody
 from PlotBlackbodies import Planck as blackbody
 import StellarModel
@@ -444,23 +444,28 @@ if emcee_import:
 
 
     class Bayesian_LS(object):
-        def __init__(self, x=1, y=1, yerr=1):
+        def __init__(self, x=1, y=1, yerr=1, param_names=None):
             """
             Class to perform a bayesian least squares fit to data with errors in only the y-axis.
 
             :param x:  A numpy ndarray with the independent variable
             :param y:  A numpy ndarray with the dependent variable
             :param yerr:  A numpy ndarray with the uncertainty in the dependent variable
+            :param param_names: An iterable of the parameter names. You MUST give this if using the
+                                multinest backend.
             """
             self.x = x
             self.y = y
             self.yerr = yerr
-
-            # Default values for a bunch of stuff
-            self.nwalkers = 100
-            self.n_burn = 200
-            self.n_prod = 1000
             self.sampler = None
+            self.samples = None
+            self.n_params = None
+            self.param_names = None
+
+            if param_names is not None:
+                self.n_params = len(param_names)
+                self.param_names = param_names
+            return
 
 
         def model(self, p, x):
@@ -496,6 +501,25 @@ if emcee_import:
             return lp + self._lnlike(pars) if np.isfinite(lp) else -np.inf
 
 
+        def mnest_prior(self, cube, ndim, nparams):
+            """
+            This pretty much MUST be overridden for any practical use!
+            Transform the 'cube' parameter, which holds everything being fit,
+            from a uniform distibution on [0,1] to the prior probability distribution.
+            (Use the inverse cumulative distribution function)
+            """
+            return
+
+
+        def mnest_lnlike(self, cube, ndim, nparams):
+            """
+            This is probably okay as it is. You may (but probably not) need to override
+            _lnlike, but not this one.
+            """
+            pars = np.array([cube[i] for i in range(nparams)])
+            return self._lnlike(pars)
+
+
         def guess_fit_parameters(self, fitorder=1):
             """
             Do a normal (non-bayesian) fit to the data.
@@ -511,10 +535,25 @@ if emcee_import:
             self.guess_pars = best_pars
             return best_pars
 
-
-        def fit(self, nwalkers=None, n_burn=None, n_prod=None, guess=True, initial_pars=None, **guess_kws):
+        def fit(self, backend='emcee', *args, **kwargs):
             """
-            Perform the full MCMC fit.
+            Perform the full MCMC fit. This function calls either fit_multinest or fit_emcee, depending on the backend.
+            See the doc-strings for those methods to learn what args and kwargs should be for each backend.
+
+            :param backend: string - either 'emcee' or 'multinest'.
+            :param args:   A list of arguments to pass to either fit_multinest or fit_emcee
+            :param kwargs: A dict of keyword arguments to pass to either fit_multinest or fit_emcee
+            :return: None
+            """
+            if backend.lower() == 'emcee':
+                return self.fit_emcee(*args, **kwargs)
+            elif backend.lower() == 'multinest':
+                return self.fit_multinest(*args, **kwargs)
+
+
+        def fit_emcee(self, nwalkers=100, n_burn=200, n_prod=1000, guess=True, initial_pars=None, **guess_kws):
+            """
+            Perform the full MCMC fit using emcee.
 
             :param nwalkers:  The number of walkers to use in the MCMC sampler
             :param n_burn:   The number of samples to discard for the burn-in portion
@@ -523,14 +562,16 @@ if emcee_import:
                              If true, it uses self.guess_fit_parameters and passes guess_kws to the function.
                              If false, it uses initial_pars. You MUST give initial_pars if guess=False!
             """
-            nwalkers = self.nwalkers if nwalkers is None else nwalkers
-            n_burn = self.n_burn if n_burn is None else n_burn
-            n_prod = self.n_prod if n_prod is None else n_prod
 
             if guess:
                 initial_pars = self.guess_fit_parameters(**guess_kws)
             elif initial_pars is None:
                 raise ValueError('Must give initial pars if guess = False!')
+
+            # Give generic parameter names so that the triangle method works
+            if self.param_names is None:
+                self.n_params = len(initial_pars)
+                self.param_names = ['c{}'.format(i) for i in range(self.n_params)]
 
             # Set up the MCMC sampler
             pars = np.array(initial_pars)
@@ -540,24 +581,100 @@ if emcee_import:
 
             # Burn-in
             print 'Running burn-in'
-            #p1, lnp, _ = sampler.run_mcmc(p0, n_burn)
             i = 0
-            for p1, lnp, _ in sampler.sample(p0, iterations=n_burn):
+            for p1, lnp, rstate in sampler.sample(p0, iterations=n_burn):
                 if i % 10 == 0:
                     logging.info('Done with burn-in iteration {} / {}'.format(i+1, n_burn))
                 i += 1
             sampler.reset()
 
             print 'Running production'
-            #sampler.run_mcmc(p1, n_prod)
             i = 0
-            for p1, lnp, _ in sampler.sample(p0, iterations=n_prod):
+            for p1, lnp, _ in sampler.sample(p0, lnprob0=lnp, rstate0=rstate, iterations=n_prod):
                 if i % 10 == 0:
                     logging.info('Done with production iteration {} / {}'.format(i+1, n_prod))
                 i += 1
 
             # Save the sampler instance as a class variable
             self.sampler = sampler
+
+            # Put the chain in a pandas array for easier access/manipulation
+            chain_dict = {self.param_names[i]: sampler.flatchain[:, i] for i in range(self.n_params)}
+            chain_dict['lnprob'] = sampler.flatlnprobability
+            self.samples = pd.DataFrame(data=chain_dict)
+            return
+
+        def fit_multinest(self, n_live_points=1000, basename='chains/single-',
+                          verbose=True, refit=False, overwrite=False,
+                          **kwargs):
+            """
+            Fits model using MultiNest, via pymultinest. This function was taken almost entirely
+            form Timothy Morton's 'isochrones' code on github.
+
+            :param n_live_points:
+                Number of live points to use for MultiNest fit.
+            :param basename:
+                Where the MulitNest-generated files will live.
+                By default this will be in a folder named `chains`
+                in the current working directory.  Calling this
+                will define a `_mnest_basename` attribute for
+                this object.
+            :param verbose:
+                Whether you want MultiNest to talk to you.
+            :param refit, overwrite:
+                Set either of these to true if you want to
+                delete the MultiNest files associated with the
+                given basename and start over.
+            :param **kwargs:
+                Additional keyword arguments will be passed to
+                :func:`pymultinest.run`.
+            """
+            # Make sure the output directory exists
+            ensure_dir(basename)
+
+            # If previous fit exists, see if it's using the same
+            # observed properties
+            prop_nomatch = False
+            propfile = '{}properties.json'.format(basename)
+            if os.path.exists(propfile):
+                with open(propfile) as f:
+                    props = json.load(f)
+                if set(props) != set(self.param_names):
+                    prop_nomatch = True
+
+            if prop_nomatch and not overwrite:
+                raise ValueError('Properties not same as saved chains ' +
+                                 '(basename {}*). '.format(basename) +
+                                 'Use overwrite=True to fit.')
+
+            if refit or overwrite:
+                files = glob.glob('{}*'.format(basename))
+                [os.remove(f) for f in files]
+
+            self._mnest_basename = basename
+
+            pymultinest.run(self.mnest_lnlike, self.mnest_prior, self.n_params,
+                            n_live_points=n_live_points, outputfiles_basename=basename,
+                            verbose=verbose,
+                            **kwargs)
+
+            with open(propfile, 'w') as f:
+                json.dump(self.param_names, f, indent=2)
+
+            self._make_mn_samples()
+
+            return
+
+        def _make_mn_samples(self):
+            """
+            Make MCMC samples out of a multinest run. MUST call fit() method before this!
+            """
+            chain = np.loadtxt('{}post_equal_weights.dat'.format(self._mnest_basename))
+
+            chain_dict = {self.param_names[i]: chain[:, i] for i in range(self.n_params)}
+            chain_dict['lnprob'] = chain[:, -1]
+
+            self.samples = pd.DataFrame(data=chain_dict)
             return
 
 
@@ -566,23 +683,23 @@ if emcee_import:
             predict the y value for the given x values. Use the N most probable MCMC chains if highest=False,
             otherwise use the first N chains.
             """
-            if self.sampler is None:
+            if self.samples is None:
                 logging.warn('Need to run the fit method before predict!')
                 return
 
             # Find the N best walkers
             if N == 'all':
-                N = self.sampler.flatchain.shape[0]
+                N = self.samples.shape[0]
             else:
-                N = min(N, self.sampler.flatchain.shape[0])
+                N = min(N, self.samples.shape[0])
 
             if highest:
-                indices = np.argsort(self.sampler.flatlnprobability)[:N]
-                pars = self.sampler.flatchain[indices]
+                samples = self.samples.sort('lnprob', ascending=False)[:N]
             else:
-                indices = np.random.randint(0, self.sampler.flatchain.shape[0], N)
-                pars = self.sampler.flatchain[indices]
+                indices = np.random.randint(0, self.samples.shape[0], N)
+                samples = self.samples.ix[indices]
 
+            pars = samples[self.param_names].as_matrix()
             y = np.array([self.model(p, x) for p in pars])
             return y
 
@@ -620,6 +737,32 @@ if emcee_import:
 
             self.sampler = MCSampler_Spoof(flatchain, flatlnprobability)
             return
+
+        def triangle(self, **kws):
+            if self.samples is None:
+                logging.warn('Need to run the fit method first!')
+                return
+
+            samples = self.samples[self.param_names].as_matrix()
+            triangle.corner(samples, labels=self.param_names, **kws)
+            return
+
+
+        @property
+        def mnest_analyzer(self):
+            """
+            PyMultiNest Analyzer object associated with fit.
+            See PyMultiNest documentation for more.
+            """
+            return pymultinest.Analyzer(self.n_params, self._mnest_basename)
+
+        @property
+        def evidence(self):
+            """
+            Log(evidence) from multinest fit
+            """
+            s = self.mnest_analyzer.get_stats()
+            return (s['global evidence'], s['global evidence error'])
 
 
 
@@ -863,7 +1006,7 @@ class Differential_RV(object):
         if guess or initial_pars is None:
             initial_pars = self.guess_fit_parameters(**guess_kws)
             logging.info('Normal fit done: pars = ')
-            logging.info(pars)
+            logging.info(initial_pars)
 
         pars = np.atleast_1d(initial_pars)
         ndim = pars.size
