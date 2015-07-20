0b1b318429a9b8a7eec79f9bd2bbd6dd14899662
kgullikson88 <kevin.gullikson@gmail.com>
2015-07-20 16:20:36 -0500

    Added function for an interpolating spline that takes error propagation into account, and a function to combine a list of xypoints.

--- HelperFunctions.py
+++ HelperFunctions.py
@@ -1032,9 +1032,91 @@ class ExtrapolatingUnivariateSpline(spline):
         return retval
 
 
+class ErrorPropagationSpline(object):
+    """
+    Does a spline fit, but returns both the spline value and associated uncertainty.
+    """
+
+    def __init__(self, x, y, yerr, N=1000, *args, **kwargs):
+        """
+        See docstring for InterpolatedUnivariateSpline
+        """
+        xx = np.vstack([x for i in range(N)]).T
+        yy = np.vstack([y + np.random.normal(loc=0, scale=yerr) for i in range(N)]).T
+        self._splines = [spline(x, yy[:, i], *args, **kwargs) for i in range(N)]
+
+    def __call__(self, x, *args, **kwargs):
+        """
+        Get the spline value and uncertainty at point(s) x. args and kwargs are passed to spline.__call__
+        :param x:
+        :return: a tuple with the mean value at x and the standard deviation
+        """
+        x = np.atleast_1d(x)
+        s = np.vstack([curve(x, *args, **kwargs) for curve in self._splines])
+        return (np.mean(s, axis=0), np.std(s, axis=0))
 
 
+def CombineXYpoints(xypts, snr=None, xspacing=None, numpoints=None, interp_order=3):
+    """
+    Function to combine a list of xypoints into a single
+      xypoint. Useful for combining several orders/chips
+      or for coadding spectra
 
+    Warning! This function is basically un-tested! It is NOT used
+    in TelFit!
+
+      ***Optional keywords***
+      snr: the spectra will be weighted by the signal-to-noise ratio
+           before adding
+      xspacing: the x-spacing in the final array
+      numpoints: the number of points in the final array. If neither
+                 numpoints nor xspacing is given, the x-spacing in the
+                 final array will be determined by averaging the spacing
+                 in each of the xypoints.
+      interp_order: the interpolation order. Default is cubic
+    """
+
+    if snr is None or type(snr) != list:
+        snr = [1.0] * len(xypts)
+
+    # Find the maximum range of the x data:
+    first = np.min([o.x[0] for o in xypts])
+    last = np.max([o.x[-1] for o in xypts])
+    avg_spacing = np.mean([(o.x[-1] - o.x[0]) / float(o.size() - 1) for o in xypts])
+
+    if xspacing is None and numpoints is None:
+        xspacing = avg_spacing
+    if numpoints is None:
+        if xspacing is None:
+            xspacing = avg_spacing
+        numpoints = (last - first) / xspacing
+    x = np.linspace(first, last, numpoints)
+    # print(x)
+
+    full_array = DataStructures.xypoint(x=x, y=np.zeros(x.size), err=np.zeros(x.size))
+    numvals = np.zeros(x.size, dtype=np.float)  # The number of arrays each x point is in
+    normalization = 0.0
+    for xypt in xypts:
+        interpolator = ErrorPropagationSpline(xypt.x, xypt.y / xypt.cont, xypt.err / xypt.cont, k=interp_order)
+        left = np.searchsorted(full_array.x, xypt.x[0])
+        right = np.searchsorted(full_array.x, xypt.x[-1], side='right')
+        if right < xypt.size():
+            right += 1
+        numvals[left:right] += 1.0
+        val, err = interpolator(full_array.x[left:right])
+        full_array.y[left:right] += val
+        full_array.err[left:right] += err ** 2
+        # print(interpolator(full_array.x[left:right]))
+        # print(full_array.x[left:right])
+        #print(xypt.x)
+        #print '\n'
+
+    # import pylab
+    # pylab.plot(full_array.x, numvals)
+    full_array.err = np.sqrt(full_array.err)
+    print np.unique(numvals)
+    full_array.y[numvals > 0] /= numvals[numvals > 0]
+    return full_array
 
 
 
