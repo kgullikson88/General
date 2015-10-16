5e221de800cb17b2802cf1109ad954c3d5b9c661
kgullikson88 <kevin.gullikson@gmail.com>
2015-10-16 16:53:11 -0500

    Added errorbar function that works similarly to native bokeh functions.

--- plottools.py
+++ plottools.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.set_style('white')
@@ -22,3 +21,42 @@ def add_spt_axis(axis, spt_values=('M5', 'M0', 'K5', 'K0', 'G5', 'G0')):
     top.set_xticklabels(spt_values)
     top.set_xlabel('Spectral Type')
     return top
+
+
+def bokeh_errorbar(fig, x, y, xerr=None, yerr=None, source=None, color='blue', point_kwargs=None, error_kwargs=None):
+    if point_kwargs is None:
+        point_kwargs = {}
+    if error_kwargs is None:
+        error_kwargs = {}
+
+    if source is not None:
+        df = source.to_df()
+        x = df[x].values
+        y = df[y].values
+        xerr = df[xerr].values if xerr is not None else xerr
+        yerr = df[yerr].values if yerr is not None else yerr
+        if 'source' not in point_kwargs:
+            point_kwargs['source'] = source
+
+    if 'color' not in point_kwargs:
+        point_kwargs['color'] = color
+    if 'color' not in error_kwargs:
+        error_kwargs['color'] = color
+
+    fig.circle(x, y, **point_kwargs)
+
+    if xerr is not None:
+        x_err_x = []
+        x_err_y = []
+        for px, py, err in zip(x, y, xerr):
+            x_err_x.append((px - err, px + err))
+            x_err_y.append((py, py))
+        fig.multi_line(x_err_x, x_err_y, **error_kwargs)
+
+    if yerr is not None:
+        y_err_x = []
+        y_err_y = []
+        for px, py, err in zip(x, y, yerr):
+            y_err_x.append((px, px))
+            y_err_y.append((py - err, py + err))
+        fig.multi_line(y_err_x, y_err_y, **error_kwargs)
