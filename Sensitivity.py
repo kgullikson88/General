3cd35c674b8acc1cc8998d3864aef5cbda77f5c2
kgullikson88 <kevin.gullikson@gmail.com>
2015-12-03 13:12:40 -0600

    Don't assume that vsini=0 is in the list of keys!

--- Sensitivity.py
+++ Sensitivity.py
@@ -957,8 +957,9 @@ def read_hdf5(hdf5_file):
     # Get the contrast. Split by group and then merge to limit the amount of calculation needed
     logging.info('Estimating the V-band contrast ratio for each trial')
     keys = [u'primary temps', u'temperature']
-    temp = df.groupby(('star')).apply(lambda df: df.loc[(df.rv == 0) & (df.vsini == 0)][keys]).reset_index()
-    logging.debug(temp[['star', 'primary_temps', 'temperature']])
+    test_vsini = df.vsini.unique()[0]
+    temp = df.groupby(('star')).apply(lambda df: df.loc[(df.rv == 0) & (df.vsini == test_vsini)][keys]).reset_index()
+    # logging.debug(temp[['star', 'primary_temps', 'temperature']])
     temp['contrast'] = temp.apply(lambda r: get_contrast(r, band='V'), axis=1)
     df = pd.merge(df, temp[['star', 'temperature', 'contrast']], on=['star', 'temperature'], how='left')
 
